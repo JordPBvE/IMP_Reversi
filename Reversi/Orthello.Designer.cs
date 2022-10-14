60cdb82d3cf401a9b80b62e416074cc7e7879d14
Jord van Eldik <jord.vaneldik@gmail.com>
2022-10-14 12:16:46 +0200

    begin

---
+++ Reversi/Orthello.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(64, 256);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 800);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(144, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(241, 118);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(64, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(78, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(64, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(54, 31);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(64, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(266, 31);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 1164);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Reversi/Orthello.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    public partial class Form1 : Form
+    {
+        //Declaratie van variabelen
+        int n = 6;
+        float textboxlengte;
+        float a;
+        int[,] speelbord;
+        bool RoodAanZet;
+        int t = 1;
+        int rood = 2;
+        int blauw = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.Paint += scherm;
+            panel2.Paint += scherm2;
+            panel1.MouseClick += klikken;
+            textboxlengte = 600/ n;
+            speelbord = new int[n, n];
+            RoodAanZet = false;
+            textBox1.Text = "2";
+            textBox2.Text = "2";
+            textBox3.Text = "Blauw begint";
+
+
+
+        }
+        public void scherm(object o, PaintEventArgs pea)
+        {
+            // leeg = 0, blauw = 1, rood = 2
+            speelbord[2,2] = 1;
+            speelbord[3,3] = 1;
+            speelbord[2,3] = 2;
+            speelbord[3,2] = 2;
+
+            Pen ZwartePen = new Pen(Color.Black);
+            Brush Rood = Brushes.Red;
+            Brush Blauw = Brushes.Blue;
+
+
+            if (t % 2 == 0)
+                RoodAanZet = true;
+            else
+                RoodAanZet = false;
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                a = textboxlengte * i;
+                pea.Graphics.DrawLine(ZwartePen, a, 0, a, 600);
+                pea.Graphics.DrawLine(ZwartePen, 0, a, 600, a);
+            }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (speelbord[i, j] == 1)
+                    {
+                        pea.Graphics.FillEllipse(Blauw, textboxlengte * (i), textboxlengte * (j), 600 / n, 600 / n);
+                    }
+                    else if (speelbord[i, j] == 2)
+                    {
+                        pea.Graphics.FillEllipse(Rood, textboxlengte * (i), textboxlengte * (j), 600 / n, 600 / n);
+                    }
+        }
+        public void scherm2(object o, PaintEventArgs pea)
+        {
+            pea.Graphics.FillEllipse(Brushes.Red, 1,1,40,40);
+            pea.Graphics.FillEllipse(Brushes.Blue, 1,41,40,40);
+        }
+
+
+        public void klikken(object o, MouseEventArgs mea)
+        {
+            try
+            {
+                int Horizontaal = (mea.X / Convert.ToInt32(textboxlengte));
+                int Vertikaal = (mea.Y / Convert.ToInt32(textboxlengte));
+
+                if (RoodAanZet == false && speelbord[Horizontaal, Vertikaal] == 0)
+                {
+                    speelbord[Horizontaal, Vertikaal] = 1;
+                    textBox3.Text = "Rood is aan zet";
+                    blauw += 1;
+                    string s2 = blauw.ToString();
+                    textBox1.Text = s2;
+                }
+                else if (RoodAanZet == true && speelbord[Horizontaal, Vertikaal] == 0)
+                {
+                    speelbord[Horizontaal, Vertikaal] = 2;
+                    textBox3.Text = "Blauw is aan zet";
+                    rood += 1;
+                    string s1 = rood.ToString();
+                    textBox2.Text = s1;
+                }
+                t += 1;
+                panel1.Invalidate();
+            }
+            catch
+            {
+                Console.WriteLine("Error, Klik in het speelveld!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
